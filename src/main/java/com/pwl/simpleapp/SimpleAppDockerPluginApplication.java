aab74f23c9872befe8a4b86ecef151af60dd251c
piotr.pawliszcze <piotr.pawliszcze@jeppesen.com>
2017-06-04 20:24:29 +0200

    Initial commit

---
+++ src/main/java/com/pwl/simpleapp/SimpleAppDockerPluginApplication.java
@@ -0,0 +1,12 @@
+package com.pwl.simpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAppDockerPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAppDockerPluginApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pwl/simpleapp/SimpleAppDockerPluginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pwl.simpleapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleAppDockerPluginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
